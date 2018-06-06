55ee980ecd8ef184410f1713e3e3c6f0d5e19b70
Ivan Malagon <hacheka@gmail.com>
2018-06-06 12:39:27 +0200

    wide line bug

--- src/index.js
+++ src/index.js
@@ -1,7 +1,40 @@
-console.log('tucutu');
+import { SSL_OP_LEGACY_SERVER_CONNECT } from "constants";
 
-const tucutu = () => {
-  console.log('yepo');
-};
+console.log('Hackarto VL!');
 
-tucutu();
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json',
+  center: [-3.7038, 40.4168],
+  zoom: 10,
+  dragRotate: false
+});
+
+carto.setDefaultAuth({
+  user: 'hacheka',
+  apiKey: 'default_public'
+});
+
+const s = carto.expressions;
+const source = new carto.source.Dataset('commuting_routes');
+let duration = s.prop('duration');
+let commuters = s.prop('commuters');
+// let torque = s.torque(timestamp, 10);
+const viz = new carto.Viz({
+  width: s.ramp(s.linear(commuters), [1, 5]),
+  color: s.ramp(s.linear(duration), s.palettes.PRISM)
+});
+const layer = new carto.Layer('layer', source, viz);
+
+layer.addTo(map);
+
+const interactivity = new carto.Interactivity(layer);
+interactivity.on('featureHover', event => {
+  if (event.features.length > 0) {
+    console.log(event);
+  }
+})
+
+window.viz = viz;
+window.source = source;
+window.s = s;
